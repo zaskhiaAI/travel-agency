8084fa5c9089e3167b10ee946601baa447012a9a
zaskhiaAI <zaskhiaartinaisnalifah@gmail.com>
2024-10-27 01:23:59 +0700

    add index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("alertMessage");
+btn.addEventListener("click", () => {
+  alert("Kamu berhasil klik button ini");
+  // console.log("Kamu Berhasil Klik Tombol Ini");
+  // btn.style.backgroundColor = "red";
+});
